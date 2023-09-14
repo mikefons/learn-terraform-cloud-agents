1bc380f530daf619fd62c5325b6452c3739e8a29
mikefons <fonseca.michael@gmail.com>
2023-09-13 22:40:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
     }
   }
 }
-
-provider "docker" {}
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
